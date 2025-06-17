63bbc11a82494c83a09c92ffcbbdfd36ca863297
0-Parth-D <89337450+0-Parth-D@users.noreply.github.com>
2025-06-17 17:52:00 +0530

    env fix 2

--- src/components/Contact Form/ContactForm.jsx
+++ src/components/Contact Form/ContactForm.jsx
@@ -18,7 +18,7 @@ export const ContactForm = () => {
 
     const form = event.target;
     const formData = new FormData(form);
-    formData.append("access_key", import.meta.env.EMAIL_KEY);
+    formData.append("access_key", process.env.EMAIL_KEY);
 
     try {
       const response = await fetch("https://api.web3forms.com/submit", {
